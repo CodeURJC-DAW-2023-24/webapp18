2adfa86819e73c36dc58c4702d1058d547829fbd
MarcosDelValle <marcosdelvah@gmail.com>
2024-02-12 18:12:12 +0100

    Creado el router reenviando peticion inicial a index

---
+++ backend/helloword-vscode/src/main/java/es/codeurjc/hellowordvscode/AppRouter.java
@@ -0,0 +1,17 @@
+package es.codeurjc.hellowordvscode;
+
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class AppRouter {
+    @GetMapping("/")
+    public String initial(Model model) {
+
+        model.addAttribute("name","World");
+        return "index";
+    }
+    
+}
