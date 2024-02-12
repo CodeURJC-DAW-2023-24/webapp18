219126c2074573b925f0f7fa79c1c44896d1ac95
MarcosDelValle <marcosdelvah@gmail.com>
2024-02-12 11:46:53 +0100

    Creado backend con el proyecto en maven

---
+++ backend/helloword-vscode/src/test/java/es/codeurjc/hellowordvscode/HellowordVscodeApplicationTests.java
@@ -0,0 +1,13 @@
+package es.codeurjc.hellowordvscode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellowordVscodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
