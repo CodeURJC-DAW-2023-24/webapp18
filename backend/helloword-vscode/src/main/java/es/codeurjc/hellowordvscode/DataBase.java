4f1613a7450fd308f2c048a2de4ccb881d57ffce
MarcosDelValle <marcosdelvah@gmail.com>
2024-02-13 10:03:54 +0100

    Added database base code

---
+++ backend/helloword-vscode/src/main/java/es/codeurjc/hellowordvscode/DataBase.java
@@ -0,0 +1,14 @@
+package es.codeurjc.hellowordvscode;
+
+import java.util.HashMap;
+
+public class DataBase {
+
+        public void startBD(){
+            //aqui meter offeers con pools y cosas 
+        }
+        
+
+}
+
+
