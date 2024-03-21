77f19a47f32f65fd8a4d80ff9f829af925ce5ae4
MarcosDelValle <marcosdelvah@gmail.com>
2024-03-21 10:17:46 +0100

    Response in chart rest controller format changed

--- backend/helloword-vscode/src/main/java/es/codeurjc/restcontroller/chartRestController.java
+++ backend/helloword-vscode/src/main/java/es/codeurjc/restcontroller/chartRestController.java
@@ -4,8 +4,11 @@ import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import es.codeurjc.DTO.OfferDTO;
 import es.codeurjc.model.Lifeguard;
 import es.codeurjc.service.UserService;
 
@@ -17,9 +20,9 @@ public class chartRestController {
     private UserService userService;
 
     @GetMapping("/api/pieChart/")
-    public HashMap<String,Integer> getPieChart(){
+    public ResponseEntity<HashMap<String,Integer>> getPieChart(){
         HashMap<String, Integer> mapa = fillAttitudes();
-        return mapa;
+        return ResponseEntity.status(HttpStatus.OK).body(mapa);
     }
      public HashMap<String, Integer> fillAttitudes() {
         HashMap<String, Integer> map = new HashMap<String, Integer>();
