93c3d5205d01d8026590d0de3995f8fe655f12f2
RisiGT <sanchez.jorge02@gmail.com>
2024-03-29 04:55:32 +0100

    Google maps dynamic + distance calculation
    Stashed since March 3-4

--- backend/helloword-vscode/src/main/java/es/codeurjc/controller/MapsController.java
+++ backend/helloword-vscode/src/main/java/es/codeurjc/controller/MapsController.java
@@ -1,23 +1,50 @@
 package es.codeurjc.controller;
 
+import java.util.Collection;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import es.codeurjc.model.Employer;
+import es.codeurjc.model.Lifeguard;
+import es.codeurjc.model.Offer;
+import es.codeurjc.service.OfferService;
+import es.codeurjc.service.PoolService;
+import es.codeurjc.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class MapsController {
+    @Autowired
+    private PoolService poolService;  // we need the pools before the offers
+
+    @Autowired
+    private OfferService offerService;
+
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/maps")
     public String maps(Model model, HttpServletRequest request) {
-
         //CHECK USER LOGED OR NOT
-        if (request.getUserPrincipal() != null){
-            model.addAttribute("loged", true);
-        }else{
-            model.addAttribute("loged", false);
+        Boolean loged = request.getUserPrincipal() != null;
+        model.addAttribute("loged", loged);
+
+        if (loged) {
+            String mail = request.getUserPrincipal().getName();
+            Optional<Lifeguard> lifeguard = userService.findLifeguardByEmail(mail);
+            Optional<Employer> employer = userService.findEmployerByEmail(mail);
+            if (lifeguard.isPresent())
+                model.addAttribute("user", lifeguard.get());
+            if (employer.isPresent())
+                model.addAttribute("user", employer.get());
         }
 
+        Collection<Offer> offers = offerService.findAll();
+        model.addAttribute("offers", offers);
         return "maps";
     }
 }
--- backend/helloword-vscode/src/main/resources/static/js/maps.js
+++ backend/helloword-vscode/src/main/resources/static/js/maps.js
@@ -1,36 +1,115 @@
 async function initMap() {
-    const center = { lat: 40.335299133707615, lng: -3.8775389329638457 };
+    center = await checkAddress(document.getElementById('user'));
+
     const map = new google.maps.Map(document.getElementById('map'), {
         zoom: 15,
         center: center,
         disableDefaultUI: true
     });
 
-    new google.maps.Marker({
+    const home = new google.maps.Marker({
         position: center,
         map: map,
-        title: 'HOME'
+        title: 'HOME',
+        icon: {
+            url: "https://cdn-icons-png.flaticon.com/512/1196/1196783.png",
+            scaledSize: new google.maps.Size(44, 44) // Aquí puedes especificar el tamaño deseado en píxeles
+        }
     });
 
-    var directions = [
-        "Piscina municipal Las Cumbres",
-        "Piscina El Soto"
-    ];
+    const bounds = new google.maps.LatLngBounds();
+    bounds.extend(home.getPosition());
 
-    var geocoder = new google.maps.Geocoder();
-    directions.forEach(function(direction) {
-        geocoder.geocode({ 'address': direction }, function(results, status) {
+    const offerElements = document.querySelectorAll('[id^="offer-"]');
+    const poolsAddress = [];
+
+    for (const element of offerElements) {
+        const address = element.textContent.trim();
+        poolsAddress.push(address);
+    }
+
+    const geocoder = new google.maps.Geocoder();
+    for (const address of poolsAddress) {
+        const [results, status] = await new Promise(resolve => {
+            geocoder.geocode({ 'address': address }, function(results, status) {
+                resolve([results, status]);
+            });
+        });
+
+        if (status == 'OK') {
+            const marker = new google.maps.Marker({
+                position: results[0].geometry.location,
+                map: map,
+                title: address
+            });
+            bounds.extend(marker.getPosition());
+        } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+        }
+    }
+
+    console.log("Calculando distancias desde HOME a:");
+    let distances = await calculateDistances(center, poolsAddress.slice(2));
+    console.log(distances);
+
+    map.fitBounds(bounds);
+}
+
+
+async function checkAddress(userAddressElement) {
+    let center = { lat: 40.335299133707615, lng: -3.8775389329638457 };  // URJC
+
+    if (userAddressElement == null || userAddressElement.textContent.trim() == '') return center;
+    const userAddress = userAddressElement.textContent.trim();
+
+    const [results, status] = await new Promise(resolve => {
+        const geocoder = new google.maps.Geocoder();
+
+        geocoder.geocode({ 'address': userAddress }, function(results, status) {
+            resolve([results, status]);
+        });
+    });
+
+    if (status === 'OK' && results && results.length > 0)
+        center = results[0].geometry.location;
+    else
+        alert('No se pudo encontrar la ubicación proporcionada.');
+
+    return center;
+}
+
+
+async function calculateDistances(center, poolsAddress) {
+    let distances = {};
+    await new Promise(resolve => {
+        const service = new google.maps.DistanceMatrixService();
+        service.getDistanceMatrix({
+            origins: [center],
+            destinations: poolsAddress,
+            travelMode: 'DRIVING',
+            unitSystem: google.maps.UnitSystem.METRIC,
+            avoidHighways: false,
+            avoidTolls: false
+        }, function(response, status) {
             if (status == 'OK') {
-                new google.maps.Marker({
-                    position: results[0].geometry.location,
-                    map: map,
-                    title: direction
-                });
+                const origins = response.originAddresses;
+                const results = response.rows[0].elements;
+                for (let i = 0; i < results.length; i++) {
+                    const element = results[i];
+                    const distance = element.distance.text;
+                    const duration = element.duration.text;
+                    const destination = poolsAddress[i];
+                    distances[destination] = {
+                        distance: distance,
+                        duration: duration
+                    };
+                }
+                resolve(distances);
             } else {
-                alert('Geocode was not successful for the following reason: ' + status);
+                console.error('Error al calcular las distancias: ' + status);
             }
         });
     });
-}
 
-initMap();
+    return distances;
+}
