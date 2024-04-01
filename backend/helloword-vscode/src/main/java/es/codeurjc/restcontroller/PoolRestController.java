530ba4f6afb8a629c188e8590a0dc3c285315967
nicorubira <nicolasdqvas@gmail.com>
2024-04-01 13:55:51 +0200

    cambio en PoolRestController para asemejar codigos

--- backend/helloword-vscode/src/main/java/es/codeurjc/restcontroller/PoolRestController.java
+++ backend/helloword-vscode/src/main/java/es/codeurjc/restcontroller/PoolRestController.java
@@ -1,116 +1,115 @@
-import java.io.IOException;
-import java.time.LocalTime;
+package es.codeurjc.restcontroller;
+
+import java.net.URI;
+import java.security.Principal;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
-import java.sql.SQLException;
 
-import org.hibernate.engine.jdbc.BlobProxy;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.InputStreamResource;
-import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import es.codeurjc.model.Message;
+import es.codeurjc.DTO.PoolDTO;
 import es.codeurjc.model.Pool;
-import es.codeurjc.service.MessageService;
 import es.codeurjc.service.PoolService;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 @RestController
+@RequestMapping("/api/pools")
 public class PoolRestController {
-
-    @Autowired
-    private MessageService messageService;
-
     @Autowired
     private PoolService poolService;
 
-// BUSCAR TODAS LAS PISCINAS
- @GetMapping("/pools")
-    public ResponseEntity<List<Pool>> getAllPools() {
+    // ----------------------------------------------- GET -----------------------------------------------
+    @Operation(summary = "Get all pools.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Pools found", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = PoolDTO.class)) }),
+            @ApiResponse(responseCode = "404", description = "Pools not found", content = @Content)
+    })
+    @GetMapping
+    public ResponseEntity<List<PoolDTO>> getPools() {
         List<Pool> pools = poolService.findAll();
-        if (!pools.isEmpty()) {
-            return ResponseEntity.ok(pools);
-        } else {
-            return ResponseEntity.notFound().build();
+        List<PoolDTO> poolDTOs = new ArrayList<>();
+
+        for (Pool pool : pools) {
+            poolDTOs.add(new PoolDTO(pool));
         }
+
+        if (poolDTOs.isEmpty())
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+
+        return ResponseEntity.status(HttpStatus.OK).body(poolDTOs);
     }
-// BUSCAR PISCINAS ESPECÍFICAS POR ID
-    @GetMapping("/pool/{id}")
-    public ResponseEntity<Pool> getPool(@PathVariable int id, HttpServletRequest request) {
+
+    @Operation(summary = "Get a pool by its ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Pool found", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = PoolDTO.class)) }),
+            @ApiResponse(responseCode = "404", description = "Pool not found, probably invalid id supplied", content = @Content)
+    })
+    @GetMapping("/{id}")
+    public ResponseEntity<PoolDTO> getPool(@PathVariable Long id) {
         Optional<Pool> pool = poolService.findById(id);
-        if (pool.isPresent()) {
-            return ResponseEntity.ok(pool.get());
-        } else {
+        if (pool.isPresent())
+            return ResponseEntity.status(HttpStatus.OK).body(new PoolDTO(pool.get()));
+        else
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
+    // ----------------------------------------------- PUT -----------------------------------------------
+    @Operation(summary = "Update a pool by its ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Pool updated", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = PoolDTO.class)) }),
+            @ApiResponse(responseCode = "400", description = "Bad request, invalid data in changes", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Pool not found, probably invalid id supplied", content = @Content)
+    })
+    @PutMapping("/{id}")
+    public ResponseEntity<PoolDTO> updatePool(@PathVariable Long id, @RequestBody PoolDTO poolDTO) {
+        Optional<Pool> poolOptional = poolService.findById(id);
+        if (!poolOptional.isPresent())
             return ResponseEntity.notFound().build();
-        }
-    }
+        Pool pool = poolOptional.get();
 
-    @PostMapping("/pool")
-    public ResponseEntity<Pool> addPool(HttpServletRequest request, HttpSession session,
-            @RequestParam("name") String name, @RequestParam("dir") String dir,
-            @RequestParam("description") String description, @RequestParam("aforo") int aforo,
-            @RequestParam("start") LocalTime start, @RequestParam("close") LocalTime close,
-            MultipartFile photoField) throws IOException {
+        // Actualizar la pool con los datos del DTO
+        pool.setName(poolDTO.getName());
+        pool.setPhoto(poolDTO.getPhoto());
+        pool.setDirection(poolDTO.getDirection());
+        pool.setCapacity(poolDTO.getCapacity());
+        // Actualizar otros campos...
 
-        Pool pool = new Pool.Builder().name(name).photo("/images/default-image.jpg").direction(dir).capacity(10)
-                .scheduleStart(start).scheduleEnd(close).company("Null").description(description).build();
-        if (!photoField.isEmpty()) {
-            pool.setPhotoUser(BlobProxy.generateProxy(photoField.getInputStream(), photoField.getSize()));
-            pool.photoCheck = true;
-        }
         poolService.save(pool);
-        return ResponseEntity.ok(pool);
+        return ResponseEntity.ok().body(new PoolDTO(pool));
     }
 
-    @DeleteMapping("/pool/{id}")
-    public ResponseEntity<Void> deletePool(@PathVariable int id, HttpServletRequest request) {
+    // ----------------------------------------------- DELETE -----------------------------------------------
+    @Operation(summary = "Delete a pool by its ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Pool deleted", content = @Content),
+            @ApiResponse(responseCode = "404", description = "Pool not found, probably invalid id supplied", content = @Content)
+    })
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePool(@PathVariable Long id) {
+        Optional<Pool> poolOptional = poolService.findById(id);
+        if (!poolOptional.isPresent())
+            return ResponseEntity.notFound().build();
+
         poolService.deleteById(id);
-        return ResponseEntity.ok().build();
-    }
-
-    @PostMapping("/pool/{id}/message")
-    public ResponseEntity<Message> newMessage(@RequestBody String input, @PathVariable int id,
-            HttpServletRequest request) {
-
-        String mail = request.getUserPrincipal().getName();
-        Message message = new Message(mail, input);
-        Pool pool = poolService.findById(id).get();
-        pool.addMessage(message);
-        messageService.save(message);
-        poolService.save(pool);
-        return ResponseEntity.ok(message);
-    }
-
-    @DeleteMapping("/pool/{id}/message/{messageId}")
-    public ResponseEntity<Void> deletePoolMessage(@PathVariable int id, @PathVariable int messageId,
-            HttpServletRequest request) {
-
-        Message msg = messageService.findById(messageId).get();
-        Pool pool = msg.getPool();
-        messageService.deleteById(messageId);
-        return ResponseEntity.ok().build();
-    }
-
-    @GetMapping("/pool/{id}/image")
-    public ResponseEntity<Resource> downloadImage(@PathVariable int id) throws SQLException {
-        Optional<Pool> pool = poolService.findById(id);
-        if (pool.isPresent() && pool.get().getPhotoUser() != null) {
-            Resource file = new InputStreamResource(pool.get().getPhotoUser().getBinaryStream());
-            return ResponseEntity.ok().header(HttpHeaders.CONTENT_TYPE, "image/jpeg")
-                    .contentLength(pool.get().getPhotoUser().length()).body(file);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+        return ResponseEntity.noContent().build();
     }
 }
