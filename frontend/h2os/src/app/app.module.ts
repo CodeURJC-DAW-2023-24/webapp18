825b15b67b301b0606b13d78632ec6963ae962e9
RisiGT <sanchez.jorge02@gmail.com>
2024-04-23 02:44:10 +0200

    Created header component
    For modularization

--- frontend/h2os/src/app/app.module.ts
+++ frontend/h2os/src/app/app.module.ts
@@ -8,9 +8,10 @@ import { routing } from './app.routing';
 import { UserLoginComponent } from './components/user/user-login.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { UserFormComponent } from './components/user/user-form.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent,UserLoginComponent,UserFormComponent],
+  declarations: [AppComponent, UserLoginComponent, UserFormComponent, HeaderComponent],
   imports: [NgbModule, BrowserModule, FormsModule, HttpClientModule, routing],
   bootstrap: [AppComponent]
 })
