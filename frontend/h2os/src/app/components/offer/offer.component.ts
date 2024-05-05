58338b14edf9e93114caae2237cf71ed12e9a9d8
RisiGT <sanchez.jorge02@gmail.com>
2024-05-05 17:16:20 +0200

    Formatted offer

--- frontend/h2os/src/app/components/offer/offer.component.ts
+++ frontend/h2os/src/app/components/offer/offer.component.ts
@@ -14,20 +14,18 @@ import { Applied } from '../../models/applied.model';
 @Component({
     selector: "offer",
     templateUrl: './offer.component.html',
-    styleUrls:['./offer.data.css','./offer.messages.css']
+    styleUrls: ['./offer.data.css', './offer.messages.css']
 })
-
-export class OfferComponent{
+export class OfferComponent {
     me: Me;
     user: Person;
     lifeguard: Lifeguard;
     owner: Employer;
-    edit:boolean;
-    typeUser:string;
+    edit: boolean;
+    typeUser: string;
     admin: boolean;
     offer: Offer;
     pool: String;
-    hasPhoto: boolean;
     canApply: boolean;
     poolName: string | undefined;
     poolPhoto: string;
@@ -37,121 +35,114 @@ export class OfferComponent{
     appliedLg: string[];
     appliedLgDesc: string[];
     photoURL: string;
-    constructor(activatedRoute: ActivatedRoute, private service: OfferService, private userService: UserService){ // Set the permits
+
+    constructor(activatedRoute: ActivatedRoute, private service: OfferService, private userService: UserService) { // Set the permits
         let id = activatedRoute.snapshot.params['id'];
         id = 1;
         this.applied = false;
         this.appliedLg = []
-        this.appliedLgDesc= []
+        this.appliedLgDesc = []
         service.getOffer(id).subscribe(
             response => {
                 this.offer = response as Offer;
-                this.hasPhoto = false;
                 this.poolName = this.offer.poolName;
                 this.poolID = this.offer.poolID;
                 service.getOfferPhoto(id).subscribe(
-                    response =>{ if(response){
-                        const blob = new Blob([response], { type: 'image/jpeg' })
-                        this.photoURL = URL.createObjectURL(blob)
-                        this.hasPhoto = true;
+                    response => {
+                        if (response) {
+                            const blob = new Blob([response], { type: 'image/jpeg' })
+                            this.photoURL = URL.createObjectURL(blob)
 
-                        userService.me().subscribe(
-                            response => {
-                                this.me = response as Me
-                                console.log(this.offer);
-                                console.log("hola")
-                                console.log(this.me);
-                                console.log(this.me.mail);
-                                this.edit = (this.me.mail=="admin" || this.me.mail==this.offer.employer)
-                                this.canApply = this.me.type=="lg";
-                                userService.getLifeguardOffers(this.me.id).subscribe(
-                                    response => {
-                                        console.log(response)
-                                        let lista = response as Offer[];
-        
-                                        for(let offerA of lista){
-                                            if(offerA.id==this.offer.id){
-                                                this.canApply = false
+                            userService.me().subscribe(
+                                response => {
+                                    this.me = response as Me
+                                    console.log(this.offer);
+                                    console.log("hola")
+                                    console.log(this.me);
+                                    console.log(this.me.mail);
+                                    this.edit = (this.me.mail == "admin" || this.me.mail == this.offer.employer)
+                                    this.canApply = this.me.type == "lg";
+                                    userService.getLifeguardOffers(this.me.id).subscribe(
+                                        response => {
+                                            console.log(response)
+                                            let lista = response as Offer[];
+
+                                            for (let offerA of lista) {
+                                                if (offerA.id == this.offer.id) {
+                                                    this.canApply = false
+                                                }
                                             }
+                                        },
+                                        error => {
+                                            console.log("Error al pedir las ofertas propuestas")
                                         }
-                        
-                                    },
-                                    error =>{ 
-                                        console.log("Error al pedir las ofertas propuestas")}
-                                );
-                                
-                            },
-                            error => {
-                                this.me.mail = "-1"
-                                this.me.type = "-1"
-                            }
-                        );
-                        
-                        
-                    }
+                                    );
+                                },
+                                error => {
+                                    this.me.mail = "-1"
+                                    this.me.type = "-1"
+                                }
+                            );
+                        }
                     },
-                    error =>{
+                    error => {
                         console.log("Error al cargar la foto")
                     });
-                
             },
             error => console.error(error)
         );
-
-        
-        
-            
-        
     }
 
-    showApplied(id: number|undefined){
+    showApplied(id: number | undefined) {
         let mapa: Applied
         this.service.getApplied(id).subscribe(
             response => {
                 console.log(response)
                 mapa = response as Applied;
 
-                if(mapa.Seleccionado!== null && mapa && mapa.Seleccionado){
+                if (mapa.Seleccionado !== null && mapa && mapa.Seleccionado) {
                     this.selected = mapa.Seleccionado[0];
                 }
                 else this.selected = "-1";
                 const intermediate2 = mapa.Propuestos;
                 console.log(intermediate2)
-                if(intermediate2){
+                if (intermediate2) {
                     this.appliedLg = intermediate2;
                 }
                 const intermediate3 = mapa.Descripciones;
-                if(intermediate3){
+                if (intermediate3) {
                     this.appliedLgDesc = intermediate3;
                 }
                 this.applied = true;
-
             },
             error => console.error(error)
         );
     }
-    deleteOffer(id: number|undefined){
+
+    deleteOffer(id: number | undefined) {
         this.service.deleteOffer(id);
     }
-    apply(id: number|undefined){
+
+    apply(id: number | undefined) {
         this.service.applyToOffer(id);
         this.canApply = false;
     }
-    setLifeguard(idOffer: number|undefined, idLg: number|undefined){
-        this.service.setLifeguard(idOffer,idLg);
+
+    setLifeguard(idOffer: number | undefined, idLg: number | undefined) {
+        this.service.setLifeguard(idOffer, idLg);
         this.applied = this.service.unSelectLifeguard(idOffer);
         this.applied = false;
-
     }
-    unSelectLifeguard(idOffer: number|undefined){
+
+    unSelectLifeguard(idOffer: number | undefined) {
         this.service.unSelectLifeguard(idOffer);
         this.applied = false;
-
     }
-    showApplied2(id: number|undefined){
+
+    showApplied2(id: number | undefined) {
         let mapa: Map<string, string[]>
         this.selected = "s1";
-        this.appliedLg = ["s1","s2","s3"];
+        this.appliedLg = ["s1", "s2", "s3"];
         this.applied = true;
     }
 }
--- frontend/h2os/src/app/services/offer.service.ts
+++ frontend/h2os/src/app/services/offer.service.ts
@@ -9,53 +9,54 @@ import { Pool } from "../models/pool.model";
 const urlOffer = '/api/offers'
 const urlLifeguards = '/lifeguards'
 
-@Injectable({ providedIn: 'root'})
-export class OfferService{
-    constructor(private httpClient: HttpClient){}
+@Injectable({ providedIn: 'root' })
+export class OfferService {
+    constructor(private httpClient: HttpClient) { }
 
-    getOffer(id: number): Observable<Object>{
-        return this.httpClient.get(urlOffer+"/"+id);
-    }
-    getOfferPhoto(id: number){
-        return this.httpClient.get(urlOffer+"/"+id+"/photo", { responseType: 'arraybuffer' });
-        
+    getOffer(id: number): Observable<Object> {
+        return this.httpClient.get(urlOffer + "/" + id);
     }
 
-    deleteOffer(id: number| undefined) {
-        this.httpClient.delete(urlOffer+"/"+id).subscribe();
-    
+    getOfferPhoto(id: number) {
+        return this.httpClient.get(urlOffer + "/" + id + "/photo", { responseType: 'arraybuffer' });
     }
 
-    applyToOffer(id: number | undefined){
-        return this.httpClient.post(urlOffer+"/"+id+"/"+urlLifeguards,"").subscribe();
-    }
-    
-    getApplied(id: number | undefined): Observable<Object>{
-        return this.httpClient.get(urlOffer+"/"+id+"/lifeguards");
+    deleteOffer(id: number | undefined) {
+        this.httpClient.delete(urlOffer + "/" + id).subscribe();
     }
 
-    setLifeguard(idOffer: number | undefined, idLg: number | undefined){
-        this.httpClient.put(urlOffer+"/"+idOffer+"/lifeguards/"+idLg,"").subscribe();
+    applyToOffer(id: number | undefined) {
+        return this.httpClient.post(urlOffer + "/" + id + "/" + urlLifeguards, "").subscribe();
+    }
+
+    getApplied(id: number | undefined): Observable<Object> {
+        return this.httpClient.get(urlOffer + "/" + id + "/lifeguards");
+    }
+
+    setLifeguard(idOffer: number | undefined, idLg: number | undefined) {
+        this.httpClient.put(urlOffer + "/" + idOffer + "/lifeguards/" + idLg, "").subscribe();
         return true
     }
 
-    unSelectLifeguard(id: number | undefined){
-        this.httpClient.delete(urlOffer+"/"+id+"/lifeguards").subscribe();
+    unSelectLifeguard(id: number | undefined) {
+        this.httpClient.delete(urlOffer + "/" + id + "/lifeguards").subscribe();
         return true
     }
 
-    editOffer(id: number | undefined, offer: Offer){
+    editOffer(id: number | undefined, offer: Offer) {
         const json = ""
         console.log(JSON.stringify(offer))
-        this.httpClient.put(urlOffer+"/"+id, offer).subscribe();
+        this.httpClient.put(urlOffer + "/" + id, offer).subscribe();
         return true
     }
-    newOffer(offer: Offer){
+
+    newOffer(offer: Offer) {
         this.httpClient.post(urlOffer, offer).subscribe();
         return true
     }
-    private handleError(error:any){
+
+    private handleError(error: any) {
         console.error(error);
-        return throwError("Server error ("+error.status+"):"+error.text())
+        return throwError("Server error (" + error.status + "):" + error.text())
     }
 }
