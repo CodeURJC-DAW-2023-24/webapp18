aa9c59ee7fccb361156876405ccab70455ddd22c
MarcosDelValle <marcosdelvah@gmail.com>
2024-05-06 16:14:28 +0200

    new mapping changes implemented

---
+++ backend/h2os/src/main/java/es/codeurjc/controller/SPAController.java
@@ -0,0 +1,14 @@
+package es.codeurjc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SPAController {
+
+    @GetMapping({ "/new/**/{path:[^\\.]*}", "/{path:new[^\\.]*}","/new/" })
+        public String redirect() {
+        return "forward:../frontend/h2os/src/index.html";
+    }
+
+}
