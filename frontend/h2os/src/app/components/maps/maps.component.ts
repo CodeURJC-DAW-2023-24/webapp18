ec7c9d0515373ceb3daa73b6fa6b44ee13a3053b
RisiGT <sanchez.jorge02@gmail.com>
2024-05-18 22:35:56 +0200

    Formatted maps.component
    Commited 5 hours ago but has been ammended

--- frontend/h2os/src/app/components/maps/maps.component.ts
+++ frontend/h2os/src/app/components/maps/maps.component.ts
@@ -15,84 +15,11 @@ import { Observable } from 'rxjs';
   styleUrls: ['./maps.component.css']
 })
 export class MapsComponent implements OnInit {
-  logged: boolean = false;
-  user: any;
   offers: any;
-  pools: any;
   center: string;
-  alternative: any;
   addresses: string[] = [];
 
-  constructor(
-    private http: HttpClient,
-    private geocodingService: GeocodingService,
-    private toastr: ToastrService
-  ) { }
-
-  ngOnInit() {
-    this.mapCenter = new google.maps.LatLng(40.335299133707615, -3.8775389329638457);  // URJC
-
-    this.getMapsDTO().subscribe(
-      response => {
-        this.center = response.center as string;
-        this.offers = response.offers as MapsOffer[];
-        this.getAddresses();
-        this.loadOffers();
-      },
-      error => alert('Error: ' + error.message)
-    );
-  }
-
-  getMapsDTO() {
-    return this.http.get<MapsResponse>('/api/maps');
-  }
-
-  getAddresses() {
-    for (const offer of this.offers) {
-      this.addresses.push(offer.address);
-    }
-  }
-
-  filterByDistance() {
-    throw new Error('Method not implemented.');
-  }
-
-  loadOffers() {
-    this.addresses.forEach(address => {
-      this.findAddress(address).subscribe(
-        (response: GeocoderResponse) => {
-          if (response.status === 'OK' && response.results?.length) {
-            const location = response.results[0];
-            const loc: any = location.geometry.location;
-  
-            this.locationCoords = new google.maps.LatLng(loc.lat, loc.lng);
-            this.coords.push(this.locationCoords);
-  
-            const marker = new google.maps.Marker({
-              position: new google.maps.LatLng(loc.lat, loc.lng),
-              map: this.map.googleMap,
-              title: location.formatted_address
-            });
-  
-            this.markers.push(marker);
-          } else {
-            this.toastr.error(response.error_message, response.status + ": " + address);
-          }
-        },
-        (err: HttpErrorResponse) => {
-          console.error('geocoder error', err);
-        }
-      )
-        .add(() => {
-          this.geocoderWorking = false;
-        });
-    });
-  }
-
-
-  @ViewChild(GoogleMap, { static: false }) map: GoogleMap;
-  @ViewChild(MapInfoWindow, { static: false }) infoWindow: MapInfoWindow;
-
+  // Google attributes
   mapZoom = 15;
   mapCenter: google.maps.LatLng;
   mapOptions: google.maps.MapOptions = {
@@ -116,28 +43,89 @@ export class MapsComponent implements OnInit {
   geocoderWorking = false;
   geolocationWorking = false;
 
-  address: string;
   formattedAddress?: string | null = null;
-  locationCoords?: google.maps.LatLng | null = null;
 
-  openInfoWindow(marker: MapMarker) {
+  @ViewChild(GoogleMap, { static: false }) map: GoogleMap;
+  @ViewChild(MapInfoWindow, { static: false }) infoWindow: MapInfoWindow;
+
+  constructor(
+    private http: HttpClient,
+    private geocodingService: GeocodingService,
+    private toastr: ToastrService
+  ) { }
+
+  ngOnInit() {
+    this.getMapsDTO().subscribe(
+      response => {
+        this.center = response.center as string;
+        this.offers = response.offers as MapsOffer[];
+        this.loadAddress(this.center);
+        this.loadOffers()
+      },
+      error => alert('Error: ' + error.message)
+    );
+  }
+
+  getMapsDTO() {
+    return this.http.get<MapsResponse>('/api/maps');
+  }
+
+  filterByDistance() {
+    throw new Error('Method not implemented.');
+  }
+
+  loadOffers() {
+    for (const offer of this.offers) {
+      const address = offer.address as string;
+      this.loadAddress(address);
+    };
+  }
+
+  loadAddress(address: string) {
+    this.findAddress(address).subscribe(
+      (response: GeocoderResponse) => {
+        if (response.status === 'OK' && response.results?.length)
+          this.setMarker(response);
+        else
+          this.toastr.error(response.error_message, response.status + ": " + address);
+      },
+      (err: HttpErrorResponse) => console.error('geocoder error', err)
+    ).add(() => {
+      this.geocoderWorking = false;
+    });
+  }
+
+  // Google methods
+  findAddress(address: string) {
+    if (!address || address.length === 0)
+      return new Observable<GeocoderResponse>(subscriber =>
+        subscriber.error('No address'));
+
+    this.geocoderWorking = true;
+    return this.geocodingService.getLocation(address)
+  }
+
+  setMarker(response: GeocoderResponse) {
+    const location = response.results[0];
+    const loc: any = location.geometry.location;
+
+    const locationCoords = new google.maps.LatLng(loc.lat, loc.lng);
+    this.coords.push(locationCoords);
+
+    const marker = new google.maps.Marker({
+      position: new google.maps.LatLng(loc.lat, loc.lng),
+      map: this.map.googleMap,
+      title: location.formatted_address
+    });
+
+    this.markers.push(marker);
+  }
+
+  public openInfoWindow(marker: MapMarker) {
     this.infoWindow.open(marker);
   }
 
   get isWorking(): boolean {
     return this.geolocationWorking || this.geocoderWorking;
   }
-
-  findAddress(address: string) {
-    this.address = address;
-    if (!this.address || this.address.length === 0) {
-      return new Observable<GeocoderResponse>(subscriber => {
-        subscriber.error('No address');
-      })
-    }
-
-    this.geocoderWorking = true;
-    return this.geocodingService.getLocation(this.address)
-  }
-
 }
