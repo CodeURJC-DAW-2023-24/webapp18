84f0c4dfededbe158bcec9770baae523941a2128
RisiGT <sanchez.jorge02@gmail.com>
2024-05-19 04:04:11 +0200

    Refactored login process
    Updated 'user-login.component' to directly navigate to the profile when the user is logged.

--- frontend/h2os/src/app/components/header/header.component.ts
+++ frontend/h2os/src/app/components/header/header.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { UserService } from '../../services/user.service';
-import { Me } from '../../models/me.model';
 
 @Component({
   selector: 'app-header',
@@ -9,23 +7,9 @@ import { Me } from '../../models/me.model';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-  me: Me | null = null;
-
-  constructor(private router: Router, private userService: UserService) { }
+  constructor(private router: Router) { }
 
   login(): void {
-    this.userService.me().subscribe(
-      response => {
-        this.me = response as Me;
-        if (this.me.type === 'lg') {
-          this.router.navigate(['lifeguards', this.me.id]);
-        } else {
-          this.router.navigate(['employers', this.me.id]);
-        }
-      },
-      _error => {
-        this.router.navigate(['login']);
-      }
-    );
+    this.router.navigate(['login']);
   }
 }
--- frontend/h2os/src/app/components/user/user-login.component.ts
+++ frontend/h2os/src/app/components/user/user-login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { UserService } from '../../services/user.service';
 import { Router } from '@angular/router';
 import { ViewChild, ElementRef } from '@angular/core';
+import { Me } from '../../models/me.model';
 
 
 @Component({
@@ -13,7 +14,18 @@ export class UserLoginComponent{
     @ViewChild('passwordInput') passwordInput: ElementRef;
     @ViewChild('mailInput') mailInput: ElementRef;
 
-    constructor(private router:Router, private service: UserService){}
+    constructor(private router:Router, private service: UserService){
+        this.service.me().subscribe(
+            response => {
+                const me = response as Me;
+                if (me.type === 'lg')
+                    this.router.navigate(['lifeguards', me.id]);
+                else
+                    this.router.navigate(['employers', me.id]);
+            },
+            _error => console.log("Not logged in")
+        );
+    }
 
     login(){
         let mail: string = this.mailInput.nativeElement.value;
